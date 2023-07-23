0322b5366b7e3037dd055f79705d573fc597ef3e
unknown <karimi.mahdieh2002@gmail.com>
2023-07-23 22:59:04 +0330

    writted firsts codes

--- 1.py
+++ 1.py
@@ -1,5 +1,28 @@
 import requests
 from bs4 import BeautifulSoup 
+import pandas
 url='https://www.honarticket.com/'
 req=requests.get(url)
 print(req)
+names=[]
+places=[]
+times=[]
+soup=BeautifulSoup(req.text,'html.parser')
+singer_name=soup.select('span.prefix small')
+for name in singer_name : 
+    s_n=name.text
+    names.append(s_n)
+place=soup.select('i.i i-location-light')
+for p in place :
+    pl=p.text
+    places.append(pl)
+time=soup.select('i.i i-calendar')
+for t in time:
+    ti=t.text
+    times.append(ti)
+concerts={'name':names,'place':places,'times':time}
+data=pandas.DataFrame.from_dict(concerts,orient='index')
+data=data.transpose()
+write=pandas.ExcelWriter('concerts.xlsx')
+data.to_excel(write)    
+write.save()
